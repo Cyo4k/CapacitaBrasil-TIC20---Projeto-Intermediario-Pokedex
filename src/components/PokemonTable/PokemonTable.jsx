1b3f816e60dd70a045348112bf3033599998af03
Cyo4k <jpauloxpo@gmail.com>
2025-01-30 17:28:29 -0300

    adicionando mais status sobre o pokemon

--- src/components/PokemonTable/PokemonTable.jsx
+++ src/components/PokemonTable/PokemonTable.jsx
@@ -7,8 +7,24 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { typeHandler } from "../../utils/TypeHandler";
 
+
+
+
+
 export default function PokemonTable({ pokemonData }) {
-    const { height, weight, types } = pokemonData;
+    const { height, weight, types, stats } = pokemonData;
+
+    const translateStatName = (statName) => {
+        const translations = {
+            'hp': 'HP',
+            'attack': 'Ataque',
+            'defense': 'Defesa',
+            'special-attack': 'Ataque Especial',
+            'special-defense': 'Defesa Especial',
+            'speed': 'Velocidade'
+        };
+        return translations[statName] || statName;
+    };
 
     return (
         <TableContainer
@@ -22,7 +38,7 @@ export default function PokemonTable({ pokemonData }) {
                             "&:last-child td, &:last-child th": { border: 0 },
                         }}
                     >
-                        <TableCell>Height </TableCell>
+                        <TableCell>Altura </TableCell>
                         <TableCell>{height + "cm"}</TableCell>
                     </TableRow>
                     <TableRow
@@ -30,7 +46,7 @@ export default function PokemonTable({ pokemonData }) {
                             "&:last-child td, &:last-child th": { border: 0 },
                         }}
                     >
-                        <TableCell>Weight</TableCell>
+                        <TableCell>Peso</TableCell>
                         <TableCell>{weight + "g"}</TableCell>
                     </TableRow>
                     <TableRow
@@ -41,6 +57,18 @@ export default function PokemonTable({ pokemonData }) {
                         <TableCell>Tipo</TableCell>
                         <TableCell>{typeHandler(types)}</TableCell>
                     </TableRow>
+                    {stats.map((stat, index) => (
+                        <TableRow
+                            key={index}
+                            sx={{
+                                "&:last-child td, &:last-child th": { border: 0 },
+                            }}
+                        >
+                            <TableCell>{translateStatName(stat.stat.name)}</TableCell>
+                            <TableCell>{stat.base_stat}</TableCell>
+                        </TableRow>
+                    ))}
+                    
                 </TableBody>
             </Table>
         </TableContainer>
